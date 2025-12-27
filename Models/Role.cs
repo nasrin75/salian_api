8656f4793cf24c258c91b81aa9bc782ecac9beeb
Nasrin Hasanpour <n75hasanpur@gmail.com>
2025-12-27 17:33:00 +0330

    add Role model

---
+++ Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace salian_api.Models
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public required string FaName { get; set; }
+        public required string EnName { get; set; }
+    }
+}
