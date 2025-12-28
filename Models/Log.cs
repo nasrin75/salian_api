6526b4096ce46cc98e7c669eb7b0ae22a29792e5
Nasrin Hasanpour <n75hasanpur@gmail.com>
2025-12-28 17:28:23 +0330

    create log model

---
+++ Models/Log.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace salian_api.Models
+{
+    public class Log
+    {
+        public long Id { get; set; }
+        public long UserID { get; set; }
+        public long EmployeeID { get; set; }
+        public int ActionTypeId { get; set; }
+        public string ModelType { get; set; }
+        public long ModelId { get; set; }
+        public string LogNumber { get; set; }
+        public IPAddress Ip { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
