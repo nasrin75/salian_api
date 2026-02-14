73cc4f621fa1142c3eba0796a2a780a7e27864ed
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-02-14 17:22:36 +0330

    add claims

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using NuGet.Common;
 using salian_api.Config;
+using salian_api.Config.Permissions;
 using salian_api.Dtos.Auth;
 using salian_api.Dtos.Otp;
 using salian_api.Entities;
@@ -11,26 +16,23 @@ using salian_api.Response;
 using salian_api.Response.Auth;
 using salian_api.Response.Otp;
 using salian_api.Routes;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace salian_api.Services
 {
-    public class AuthService(ApplicationDbContext _dbContext, IOptions<AuthSettings> _authSetting) : IAuthService
+    public class AuthService(ApplicationDbContext _dbContext, IOptions<AuthSettings> _authSetting)
+        : IAuthService
     {
-
         public async Task<BaseResponse<LoginResponse>> Login(LoginDto request)
         {
-            var user = await _dbContext.Users
-            .Include(x => x.Role)
-            .FirstOrDefaultAsync(x => x.Username == request.Username && x.Password == request.Password);
+            var user = await _dbContext
+                .Users.Include(x => x.Role)
+                .FirstOrDefaultAsync(x =>
+                    x.Username == request.Username && x.Password == request.Password
+                );
 
             // return null if user not found
             if (user == null)
                 return new BaseResponse<LoginResponse>(null, 400, "USER_NOT_FOUND");
-
-            
 
             // var claims = new ClaimsIdentity();
             // foreach (var permission in user.Permissions)
@@ -52,53 +54,33 @@ namespace salian_api.Services
             //     }
             // }
 
-/*            var permissions = await GetUserPermissions(user.Id);
-
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Name, user.Username),
-                new Claim(ClaimTypes.Role, user.Role.Name)
-            };
-
-            foreach (var permission in permissions)
-            {
-                claims.Add(new Claim("Permission", permission));
-            }*/
-            // claims.AddClaims(new[]
-            // {
-            //        new Claim(JwtRegisteredClaimNames.Sub, user.Username),
-            //        new Claim(JwtRegisteredClaimNames.Jti, user.Id.ToString()),
-            //        new Claim(ClaimTypes.Role,user.Role.EnName)
-            //    });
-
-
             var token = GenerateJwtToken(user);
 
             var response = new LoginResponse
             {
                 UserId = user.Id,
+                Role = user.Role.EnName,
                 Username = request.Username,
                 Token = token,
             };
 
-
             return new BaseResponse<LoginResponse>(response);
         }
 
-
-        // public async Task<BaseResponse<OtpResponse>> SendOtp(SendOtpDto request)
         public async Task<BaseResponse<OtpResponse>> SendOtp(SendOtpDto request)
         {
-            var user = await _dbContext.Users.Where(x => (
-            x.Username == request.Username ||
-                    x.Email == request.Username ||
-                    x.Mobile == request.Username
-                    ))
+            var user = await _dbContext
+                .Users.Where(x =>
+                    (
+                        x.Username == request.Username
+                        || x.Email == request.Username
+                        || x.Mobile == request.Username
+                    )
+                )
                 .FirstOrDefaultAsync();
 
-            if (user == null) return new BaseResponse<OtpResponse>(null, 400, "USER_NOT_FOUND");
-            //return new BaseResponse<UserEntity>(user, 400, "USER_NOT_FOUND");
+            if (user == null)
+                return new BaseResponse<OtpResponse>(null, 400, "USER_NOT_FOUND");
 
             foreach (var item in user.LoginTypes)
             {
@@ -110,27 +92,27 @@ namespace salian_api.Services
             return new BaseResponse<OtpResponse>(null, 200, "SENT_OTP");
         }
 
-   /*     public async Task<List<string>> GetUserPermissions(long userId)
-        {
-            var user = await _dbContext.Users
-                .Include(u => u.Role)
-                    .ThenInclude(r => r.RolePermissions)
-                        .ThenInclude(rp => rp.Permission)
-                .Include(u => u.UserPermissions)
-                    .ThenInclude(up => up.Permission)
-                .FirstOrDefaultAsync(u => u.Id == userId);
+        /*     public async Task<List<string>> GetUserPermissions(long userId)
+             {
+                 var user = await _dbContext.Users
+                     .Include(u => u.Role)
+                         .ThenInclude(r => r.RolePermissions)
+                             .ThenInclude(rp => rp.Permission)
+                     .Include(u => u.UserPermissions)
+                         .ThenInclude(up => up.Permission)
+                     .FirstOrDefaultAsync(u => u.Id == userId);
 
-            var rolePermissions = user.Role.RolePermissions
-                .Select(rp => rp.Permission.Name);
+                 var rolePermissions = user.Role.RolePermissions
+                     .Select(rp => rp.Permission.Name);
 
-            var userPermissions = user.UserPermissions
-                .Select(up => up.Permission.Name);
+                 var userPermissions = user.UserPermissions
+                     .Select(up => up.Permission.Name);
 
-            return rolePermissions
-                .Union(userPermissions)
-                .Distinct()
-                .ToList();
-        }*/
+                 return rolePermissions
+                     .Union(userPermissions)
+                     .Distinct()
+                     .ToList();
+             }*/
 
         private string GenerateJwtToken(UserEntity user)
         {
@@ -139,29 +121,43 @@ namespace salian_api.Services
             var key = Encoding.ASCII.GetBytes(_authSetting.Value.Secret);
             var expireDate = DateTime.UtcNow.AddMinutes(30);
 
-            // add claims
-            Console.WriteLine("rolwee:" + user.Role.EnName);
-            string roleName = user.Role.EnName;
             var claims = new ClaimsIdentity();
-            claims.AddClaims(new[]
+            // add claims
+            foreach (var permission in user.Permissions)
             {
-                  new Claim(JwtRegisteredClaimNames.Sub, user.Username),
-                  new Claim(JwtRegisteredClaimNames.Jti, user.Id.ToString()),
-                  new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
-                  new Claim(ClaimTypes.Email, user.Email!),
-                  new Claim(ClaimTypes.MobilePhone, user.Mobile!),
-                  new Claim(ClaimTypes.Role,roleName)
-            });
+                claims.AddClaims(new[] { new Claim(Permissions.Permission, permission.Name) });
+            }
+
+            // var rolePermissions = _dbContext
+            //     .Roles.Include(r => r.Permissions)
+            //     .FirstOrDefaultAsync(role => role.Id == user.RoleId)
+            //     .Permissions;
+
+            foreach (var rolePermission in user.Role.Permissions)
+            {
+                claims.AddClaims(new[] { new Claim(Permissions.Permission, rolePermission.Name) });
+            }
+            // claims.AddClaims(
+            //     new[]
+            //     {
+            //         new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            //         new Claim(ClaimTypes.Name, user.Username!),
+            //         new Claim(ClaimTypes.Email, user.Email!),
+            //         new Claim(ClaimTypes.MobilePhone, user.Mobile!),
+            //         new Claim(ClaimTypes.Role, user.Role.EnName.ToString()),
+            //     }
+            // );
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = claims,
                 Expires = expireDate,
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
-                    SecurityAlgorithms.HmacSha256Signature)
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
             };
-            var token =  tokenHandler.CreateToken(tokenDescriptor);
-            
+            var token = tokenHandler.CreateToken(tokenDescriptor);
 
             return tokenHandler.WriteToken(token);
         }
