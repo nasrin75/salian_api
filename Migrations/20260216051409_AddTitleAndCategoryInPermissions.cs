a0ba94dc3e77352ed5801cee3b5785588f968da1
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-02-16 16:45:41 +0330

    add new filed in permission

---
+++ Migrations/20260216051409_AddTitleAndCategoryInPermissions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace salian_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitleAndCategoryInPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Permissions",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Permissions",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Permissions");
+        }
+    }
+}
