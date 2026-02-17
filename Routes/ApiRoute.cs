3a263edcffd108974e29d828257ff5d0931e041d
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-02-17 16:38:31 +0330

    delete test route

--- Routes/ApiRoute.cs
+++ Routes/ApiRoute.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using salian_api.Dtos.Otp;
-using salian_api.Entities;
 using salian_api.Interface;
 using salian_api.Response;
 using salian_api.Response.Otp;
@@ -49,21 +47,5 @@ public static class ApiRoute
                 }
             )
             .WithTags("Otp");
-
-        app.MapGet("/test", (ApplicationDbContext _dbContex) =>
-        {
-            return  _dbContex.Permissions.AsNoTracking()
-                .GroupBy(x => x.Category)
-                //.OrderByDescending(x => x.Id)
-               /* .Select(l => new PermissionResponse
-                {
-                    Id = l.Id,
-                    Name = l.Name,
-                    Title = l.Title,
-                    Category = l.Category,
-                })*/
-
-                .ToListAsync();
-        });
     }
 }
