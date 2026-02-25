6b854eff179f2d281e00b23bf0043d078e26a0df
Nasrin Hasanpour <n75hasanpur@gmail.com>
2026-02-25 16:25:36 +0330

    Create HistorySearchParamsDto.cs

---
+++ Dtos/History/HistorySearchParamsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace salian_api.Dtos.History
+{
+    public class HistorySearchParamsDto
+    {
+        public long? EntityId { get; set; }
+        public string? EntityName { get; set; }
+    }
+}
